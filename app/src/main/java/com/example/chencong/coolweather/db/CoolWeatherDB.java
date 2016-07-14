f929fca96129261a77c3fe45f5ceeb7c536563a8
chencong <1042738887@qq.com>
2016-07-14 10:18:10 +0800

    县区名称显示

--- app/src/main/java/com/example/chencong/coolweather/db/CoolWeatherDB.java
+++ app/src/main/java/com/example/chencong/coolweather/db/CoolWeatherDB.java
@@ -127,7 +127,7 @@ public class CoolWeatherDB {
                 County county = new County();
                 county.setId(cursor.getInt(cursor.getColumnIndex("id")));
                 county.setCountyName(cursor.getString(cursor.getColumnIndex("county_name")));
-                county.setCountyName(cursor.getString(cursor.getColumnIndex("county_code")));
+                county.setCountyCode(cursor.getString(cursor.getColumnIndex("county_code")));
                 county.setCityId(cityId);
                 list.add(county);
             }while (cursor.moveToNext());
